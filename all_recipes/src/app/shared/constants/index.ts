556d551130f82a3275d5facdd5b96b08f6c07b0a
Elie Bissor <eliebiss.26@gmail.com>
2024-02-29 15:48:17 +0100

    feat: Add images object in constants

---
+++ all_recipes/src/app/shared/constants/index.ts
@@ -0,0 +1,25 @@
+import { ImageType } from "../types";
+
+/*------------------
+Icons
+------------------*/
+export const SEARCH_ICON: ImageType = {
+  src: "/icons/search-icon.svg",
+  alt: "Search icon",
+  width: 20,
+  height: 20,
+};
+
+export const HAMBURGER_ICON: ImageType = {
+  src: "/icons/hamburger-icon.svg",
+  alt: "Hamburger icon",
+  width: 20,
+  height: 20,
+};
+
+export const USER_ICON: ImageType = {
+  src: "/icons/user-icon.svg",
+  alt: "User icon",
+  width: 20,
+  height: 20,
+};
