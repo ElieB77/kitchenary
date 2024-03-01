d0269cf5855c77ec8a6df76c520fb22223bb825e
Elie Bissor <eliebiss.26@gmail.com>
2024-03-01 11:19:58 +0100

    feat: Add icons + img data in constants.

---
+++ all_recipes/src/app/features/recipes/constants/index.ts
@@ -0,0 +1,22 @@
+import { ImageType } from "@/app/shared/types";
+
+export const NOODLES_IMG: ImageType = {
+  src: "/images/noodles.jpeg",
+  alt: "Noodles",
+  width: 500,
+  height: 500,
+};
+
+export const HEART_ICON: ImageType = {
+  src: "/icons/heart-icon.png",
+  alt: "Heart",
+  width: 25,
+  height: 25,
+};
+
+export const HEART_FILLED_ICON: ImageType = {
+  src: "/icons/heart-filled-icon.png",
+  alt: "Heart filled",
+  width: 20,
+  height: 20,
+};
