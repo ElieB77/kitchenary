af8cc1e82bf01d2efc0081a19b2598052bb77bb6
Elie Bissor <eliebiss.26@gmail.com>
2024-03-02 13:18:14 +0100

    feat: Add featuredCard cmp.

---
+++ all_recipes/src/app/features/recipes/components/organisms/FeaturedCard/index.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import Button, { ButtonProps } from "@/app/shared/components/atoms/Button";
+
+export interface FeaturedCardProps extends ButtonProps {
+  title: string;
+  count: number;
+  totalCount: string | number;
+  image: any;
+}
+
+const FeaturedCard: FC<FeaturedCardProps> = ({
+  title,
+  count,
+  totalCount,
+  image,
+  text,
+  icon,
+}) => {
+  return (
+    <div className={styles.featuredCard}>
+      <div className={styles.featuredCard__heading}>
+        <div className={styles.featuredCard__heading__count}>
+          <h2>{count}</h2>
+          <span>{totalCount}</span>
+        </div>
+        <h2>{title}</h2>
+      </div>
+      <Button text={text} icon={icon} />
+      <div className={styles.featuredCard__image}>
+        <Image {...image} />
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCard;
