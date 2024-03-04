2c7697e6b2b1dc72fdea1a04d9fa819f558e6051
Elie Bissor <eliebiss.26@gmail.com>
2024-03-04 14:17:14 +0100

    refactor: Make featured route dynamic

---
+++ all_recipes/src/app/(routes)/featured/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import FeaturedCard from "@/app/features/recipes/components/molecules/FeaturedCard";
+import FeaturedPage from "@/app/features/recipes/components/templates/FeaturedPage";
+import { NOODLES_IMG } from "@/app/features/recipes/constants";
+import FeaturedContainer from "@/app/features/recipes/containers/FeaturedContainer";
+import { RIGHT_ARROW_ICON } from "@/app/shared/constants";
+
+export default function Featured({ params }: { params: { slug: string } }) {
+  return (
+    <>
+      <FeaturedContainer slug={params.slug} />
+    </>
+  );
+}
