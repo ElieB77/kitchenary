8785d6a8686c45a20ef9a535a978881cbc086b8c
Elie Bissor <eliebiss.26@gmail.com>
2024-03-05 16:40:53 +0100

    feat: Add pagination buttons cmp.

---
+++ all_recipes/src/app/features/search/components/molecules/PaginationButtons/index.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import PaginationButton, {
+  PaginationButtonProps,
+} from "../../atoms/PaginationButton";
+import styles from "./styles.module.scss";
+
+export interface PaginationButtonsProps extends PaginationButtonProps {
+  totalPages: number;
+  currentPage: number;
+}
+
+const PaginationButtons: FC<PaginationButtonsProps> = ({
+  totalPages,
+  currentPage,
+  onClick,
+}) => {
+  return (
+    <div className={styles.paginationButtons}>
+      {Array.from({ length: totalPages }).map((el: any, index: number) => {
+        return (
+          <PaginationButton
+            onClick={onClick}
+            key={index}
+            content={index + 1}
+            active={currentPage + 1 === index + 1}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default PaginationButtons;
