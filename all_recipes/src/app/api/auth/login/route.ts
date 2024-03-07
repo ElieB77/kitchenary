f6211a5d336c8f831c3e414850137a96ac991fce
Elie Bissor <eliebiss.26@gmail.com>
2024-03-07 16:47:32 +0100

    feat: Add login api route.

---
+++ all_recipes/src/app/api/auth/login/route.ts
@@ -0,0 +1,58 @@
+import { User } from "@/app/shared/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "Invalid email or password",
+        },
+        { status: 401 }
+      );
+    }
+
+    if (!user.isVerified) {
+      return NextResponse.json(
+        { message: "User is not verified" },
+        { status: 403 }
+      );
+    }
+
+    const passwordMatch = await bcryptjs.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return NextResponse.json({
+        message: "Invalid email or password",
+        status: "401",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    return NextResponse.json(
+      { message: "User succesfully logged in!", token: token },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
