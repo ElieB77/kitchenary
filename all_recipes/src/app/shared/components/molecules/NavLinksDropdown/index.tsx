57801007d52d6fab558d5b06a0bdde5772a76883
Elie Bissor <eliebiss.26@gmail.com>
2024-03-13 21:12:44 +0100

    feat: Add nav links dropdown component.

---
+++ all_recipes/src/app/shared/components/molecules/NavLinksDropdown/index.tsx
@@ -0,0 +1,99 @@
+import {
+  CUISINES,
+  DIETS,
+  INGREDIENTS,
+  MEALS,
+  NAV_DROPDOWN_BTN,
+} from "@/app/shared/constants";
+import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
+import { renderNavigationLinks } from "@/app/shared/utils";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const NavLinksDropdown = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [openMenu, setOpenMenu] = useState<any>(null);
+  let timeout: ReturnType<typeof setTimeout>;
+
+  const toggleDropdown = (menuName: string) => {
+    setOpenMenu(openMenu === menuName ? null : menuName);
+  };
+
+  const closeDropdown = () => {
+    timeout = setTimeout(() => {
+      setOpenMenu(null);
+    }, 200);
+  };
+
+  const cancelClose = () => {
+    clearTimeout(timeout);
+  };
+
+  useEffect(() => {
+    setOpenMenu(null);
+  }, [searchParams, pathname]);
+
+  return (
+    <div className={styles.navLinksDesktop}>
+      <div className={styles.navLinksDesktop__buttons}>
+        {NAV_DROPDOWN_BTN.map((button: any) => {
+          return (
+            <button
+              key={button.id}
+              onMouseEnter={() => {
+                toggleDropdown(button.name);
+                cancelClose();
+              }}
+              onMouseLeave={closeDropdown}
+            >
+              {button.name}
+            </button>
+          );
+        })}
+      </div>
+
+      <div
+        onMouseEnter={cancelClose}
+        onMouseLeave={closeDropdown}
+        className={`${styles.navLinksDesktop__meals} ${
+          openMenu === "meals" ? styles.show : ""
+        }`}
+      >
+        {renderNavigationLinks(MEALS, "type")}
+      </div>
+
+      <div
+        onMouseEnter={cancelClose}
+        onMouseLeave={closeDropdown}
+        className={`${styles.navLinksDesktop__cuisines} ${
+          openMenu === "cuisines" ? styles.show : ""
+        }`}
+      >
+        {renderNavigationLinks(CUISINES, "cuisine")}
+      </div>
+
+      <div
+        onMouseEnter={cancelClose}
+        onMouseLeave={closeDropdown}
+        className={`${styles.navLinksDesktop__ingredients} ${
+          openMenu === "ingredients" ? styles.show : ""
+        }`}
+      >
+        {renderNavigationLinks(INGREDIENTS, "includeIngredients")}
+      </div>
+
+      <div
+        onMouseEnter={cancelClose}
+        onMouseLeave={closeDropdown}
+        className={`${styles.navLinksDesktop__diets} ${
+          openMenu === "diets" ? styles.show : ""
+        }`}
+      >
+        {renderNavigationLinks(DIETS, "diet")}
+      </div>
+    </div>
+  );
+};
+
+export default NavLinksDropdown;
